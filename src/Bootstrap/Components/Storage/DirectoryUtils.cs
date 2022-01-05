16000327a3185c9aba48b138b17854cb0f7266ae
Jax <jax.zhu@aihuishou.com>
2022-01-05 18:24:47 +0800

    update

--- src/Bootstrap/Components/Storage/DirectoryUtils.cs
+++ src/Bootstrap/Components/Storage/DirectoryUtils.cs
@@ -57,5 +57,20 @@ namespace Bootstrap.Components.Storage
         {
             Merge(new DirectoryInfo(source), new DirectoryInfo(target));
         }
+
+        public static void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            //Now Create all of the directories
+            foreach (var dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (var newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+            }
+        }
     }
 }
