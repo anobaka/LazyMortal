85611707e02e6d27fd984ddc45b7cffb672a6808
Jax <jax.zhu@aihuishou.com>
2022-02-28 18:23:51 +0800

    update

---
+++ src/Bootstrap/Components/Orm/Abstractions/IResourceService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bootstrap.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bootstrap.Components.Orm.Abstractions
+{
+    /// <summary>
+    /// todo: 
+    /// </summary>
+    internal interface IResourceService<out TDbContext, TResource, in TKey>
+        where TDbContext : DbContext where TResource : class
+    {
+        IServiceProvider ServiceProvider { get; }
+        TDbContext DbContext => ServiceProvider.GetRequiredService<TDbContext>();
+        IResourceService<TDbContext, TResource, TKey> UseNewScope();
+
+        Task<TResource> GetByKey(TKey key)
+        {
+            var exp = ExpressionExtensions.BuildKeyEqualsExpression<TResource>(key);
+            return DbContext.Set<TResource>().FirstOrDefaultAsync(exp);
+        }
+
+        Task<TResource[]> GetByKeys(IEnumerable<TKey> keys)
+        {
+            var exp = ExpressionExtensions.BuildKeyContainsExpression<TResource>(keys.Distinct().Cast<object>()
+                .ToArray());
+            return GetAll(exp);
+        }
+
+        Task<TResource[]> GetAll(Expression<Func<TResource, bool>> selector,
+            bool asNoTracking = false)
+        {
+            IQueryable<TResource> query = DbContext.Set<TResource>();
+            if (selector != null)
+            {
+                query = query.Where(selector);
+            }
+
+            if (asNoTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return query.ToArrayAsync();
+        }
+    }
+}
---
+++ src/Bootstrap/Components/Orm/Abstractions/ResourceService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bootstrap.Components.Orm.Abstractions
+{
+    internal class ResourceService<TDbContext, TResource, TKey> : IResourceService<TDbContext, TResource, TKey>
+        where TDbContext : DbContext where TResource : class
+    {
+        public IServiceProvider ServiceProvider { get; }
+
+        public ResourceService(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public IResourceService<TDbContext, TResource, TKey> UseNewScope() =>
+            new ResourceService<TDbContext, TResource, TKey>(ServiceProvider.CreateScope().ServiceProvider);
+    }
+}
