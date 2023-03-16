c0040866e84c6a03351ac9b9a64985999c44a257
jax <jax.zhu@aihuishou.com>
2023-03-16 20:17:43 +0800

    add refwrapper

---
+++ src/Bootstrap/Models/RefWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrap.Models
+{
+    public class RefWrapper<T>
+    {
+        public T Value;
+        public RefWrapper(T value) => Value = value;
+    }
+}
