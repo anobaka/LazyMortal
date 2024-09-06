8d18414c1577e337c33a3d2dc96ae91bb02233fa
anobaka <anobaka@qq.com>
2024-09-06 19:04:32 +0800

    add memory options manager;

---
+++ src/Bootstrap/Components/Configuration/MemoryOptionsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Bootstrap.Components.Configuration.Abstractions;
+
+namespace Bootstrap.Components.Configuration;
+
+public class MemoryOptionsManager<TOptions> : AbstractOptionsManager<TOptions> where TOptions : class, new()
+{
+    private TOptions _options;
+    public override TOptions Value => _options;
+
+    public override void Save(TOptions options)
+    {
+        _options = options;
+    }
+
+    public override async Task SaveAsync(TOptions options)
+    {
+        _options = options;
+    }
+}
