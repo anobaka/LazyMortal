9342b5a645bed08b75274999d1135801a113a112
Jax Lecter <anobaka@qq.com>
2025-06-19 23:35:19 +0800

    remove empty combination

--- src/Bootstrap/Extensions/ArrayExtensions.cs
+++ src/Bootstrap/Extensions/ArrayExtensions.cs
@@ -8,7 +8,7 @@ public static class ArrayExtensions
     public static IEnumerable<IEnumerable<T>> GetAllCombinations<T>(this T[] array)
     {
         var count = 1 << array.Length; // 2^n combinations
-        for (var i = 0; i < count; i++)
+        for (var i = 1; i < count; i++)
         {
             var combination = array.Where((t, j) => (i & (1 << j)) != 0).ToList();
             yield return combination;
