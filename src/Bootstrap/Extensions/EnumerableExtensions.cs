a515be2a660158ab50bfc926ad3487ea3257e4ea
anobaka <anobaka@qq.com>
2025-08-22 18:35:10 +0800

    enhance intelligence

--- src/Bootstrap/Extensions/EnumerableExtensions.cs
+++ src/Bootstrap/Extensions/EnumerableExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Bootstrap.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T>? e) => e == null || !e.Any();
-        public static bool IsNotEmpty<T>(this IEnumerable<T>? e) => e?.Any() == true;
+        public static bool IsNullOrEmpty<T>([NotNullWhen(true)] this IEnumerable<T>? e) => e == null || !e.Any();
+        public static bool IsNotEmpty<T>([NotNullWhen(true)] this IEnumerable<T>? e) => e?.Any() == true;
     }
 }
