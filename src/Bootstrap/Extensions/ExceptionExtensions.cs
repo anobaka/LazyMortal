1d127e7701353d6538a8a17dc1ec3924cedd2d33
anobaka <anobaka@qq.com>
2026-05-29 10:11:53 +0800

    arch: format exception text

--- src/Bootstrap/Extensions/ExceptionExtensions.cs
+++ src/Bootstrap/Extensions/ExceptionExtensions.cs
@@ -6,41 +6,10 @@ namespace Bootstrap.Extensions
 {
     public static class ExceptionExtensions
     {
-        private static IEnumerable<string> _getExceptionInformationRecursively(Exception? e, int level = 0)
-        {
-            if (e != null)
-            {
-                var info = new List<string>
-                {
-                    $"{(level == 0 ? "message" : "inner1 message")}: {e.Message}",
-                    $"{(level == 0 ? "stacktrace" : "inner1 stacktrace")}: {e.StackTrace}",
-                };
-                if (e.InnerException != null)
-                {
-                    info.AddRange(_getExceptionInformationRecursively(e.InnerException, ++level));
-                }
+        [Obsolete("Use Exception.ToString() instead.")]
+        public static string BuildFullInformationText(this Exception e) => e.ToString();
 
-                return info;
-            }
-
-            return [];
-        }
-
-        public static string BuildFullInformationText(this Exception e)
-        {
-            return string.Join(Environment.NewLine, _getExceptionInformationRecursively(e));
-        }
-
-        public static string BuildAllMessages(this Exception e)
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine(e.Message);
-            if (e.InnerException != null)
-            {
-                sb.AppendLine($"->{BuildAllMessages(e.InnerException)}");
-            }
-
-            return sb.ToString();
-        }
+        [Obsolete("Use Exception.ToString() instead.")]
+        public static string BuildAllMessages(this Exception e) => e.ToString();
     }
 }
